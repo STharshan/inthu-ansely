0b08c139577400734d3b86bd41691f06ae436faf
shandiru <msddiru@gmail.com>
2026-01-06 23:43:28 +0530

    added

--- src/components/Home/Animation.jsx
+++ src/components/Home/Animation.jsx
@@ -80,14 +80,14 @@ const Performance = () => {
       </div>
 
       <div className="content">
-        <p>
+        {/* <p>
           Run graphics-intensive workflows with a responsiveness that keeps up with your
           imagination. The M4 family of chips features a GPU with a second-generation
           hardware-accelerated ray tracing engine that renders images faster, so{" "}
           <span className="text-white">gaming feels more immersive and realistic than ever.</span>{" "}
           And Dynamic Caching optimizes fast on-chip memory to dramatically increase average GPU
           utilization — driving a huge performance boost for the most demanding pro apps and games.
-        </p>
+        </p> */}
       </div>
     </section>
   );
